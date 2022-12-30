bbfa5250f65771de81ad77d2350b70d99b6b4249
пк <@DESKTOP-70U3VC3>
2022-12-30 13:39:53 +0200

    Добавьте файлы проекта.

---
+++ Authorization/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authorization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string _login = "Anastasia";
+        string _password = "12345";
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+            MessageBoxResult res = MessageBoxResult.Yes;
+            if (Login.Text == _login && Password.Password == _password)
+            {
+                //Переход на другую страницу
+            }
+            else
+            {
+                res = MessageBox.Show("Unsuccessfully\nTry again", "Authorization", MessageBoxButton.YesNo);
+
+                if (res == MessageBoxResult.Yes)
+                {
+                    Login.Text = "";
+                    Password.Password = "";
+                }
+                else
+                {
+                    // переход на предыдущую страницу
+                }
+            }
+
+
+        }
+    }
+}
